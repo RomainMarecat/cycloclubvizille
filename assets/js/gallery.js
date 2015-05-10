f768f470a7a0e53305780e9082d02e4f91c0bc1a
Romain Marecat <romain.marecat@gmail.com>
2015-05-10 16:41:13 +0200

    added new bar on portfolio

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -38,25 +38,6 @@ $(document).ready(function() {
 	$('#home .container').css({ 'opacity' : (1.4 - st/400) });
 	
 	/*============================================
-	Navigation Functions
-	==============================================*/
-	if ($(window).scrollTop()< ($(window).height()-50)){
-		$('#main-nav').removeClass('scrolled');
-	}
-	else{
-		$('#main-nav').addClass('scrolled');    
-	}
-
-	$(window).scroll(function(){
-		if ($(window).scrollTop()< ($(window).height()-50)){
-			$('#main-nav').removeClass('scrolled');
-		}
-		else{
-			$('#main-nav').addClass('scrolled');    
-		}
-	});
-	
-	/*============================================
 	ScrollTo Links
 	==============================================*/
 	$('a.scrollto').click(function(e){
