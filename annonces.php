0a4cc41a820c9623aa1d57fbf35315fb8464e2de
Romain Marecat <romain.marecat@gmail.com>
2015-08-02 22:31:31 +0200

    fixed image

--- annonces.php
+++ annonces.php
@@ -40,6 +40,25 @@ if(isset($_POST['submitted'])) {
 			$description = trim($_POST['contactDescription']);
 		}
 	}
+	if(!isset($_FILES['contactImage'])) {
+		$imageError = "Vous avez oublié de rentrer une image de l'objet.";
+		$hasError = true;
+	} else {
+		$image 			  = $_FILES['contactImage'];
+		$file_tmp_name    = $_FILES['contactImage']['tmp_name'];
+	    $file_name        = $_FILES['contactImage']['name'];
+	    $file_size        = $_FILES['contactImage']['size'];
+	    $file_type        = $_FILES['contactImage']['type'];
+	    $file_error       = $_FILES['contactImage']['error'];
+	    if ($file_error > 0) {
+			$hasError = true;
+	    }
+	    //read from the uploaded file & base64_encode content for the mail
+	    $handle = fopen($file_tmp_name, "r");
+	    $content = fread($handle, $file_size);
+	    fclose($handle);
+	    $encoded_content = chunk_split(base64_encode($content));
+	}
 	if(trim($_POST['contactPrice']) === '') {
 		$priceError = "Vous avez oublié de rentrer un prix pour l'objet.";
 		$hasError = true;
@@ -77,7 +96,33 @@ if(isset($_POST['submitted'])) {
 		$sendCopy = trim($_POST['sendCopy']);
 		$body = "Name: $name \nEmail: $email \nTitre: $title \nDescription: $description \nPrix: $price \nCode postal: $codePostal \nVille: $city \n
 A integrer dans le fichier annonces.json: \n{\nname: $name,\nemail: $email,\ntitle: $title,\ndescription: $description,\nprice: $price,\ncodePostal: $codePostal,\ncity: $city\n}";
-		$headers = 'From: ' .' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
+		// $headers = 'From: ' .' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+		# Mail headers should work with most clients (including thunderbird)
+	    $headers = "MIME-Version: 1.0\r\n";
+	    $headers .= "X-Mailer: PHP/" . phpversion()."\r\n";
+	    $headers .= "From:".$email."\r\n";
+	    $headers .= "Subject:".$subject."\r\n";
+	    $headers .= "Reply-To: ".$email."" . "\r\n";
+	    $headers .= "Content-Type: multipart/mixed; boundary=".md5('boundary1')."\r\n\r\n";
+
+	    $headers .= "--".md5('boundary1')."\r\n";
+	    $headers .= "Content-Type: multipart/alternative;  boundary=".md5('boundary2')."\r\n\r\n";
+	    
+	    $headers .= "--".md5('boundary2')."\r\n";
+	    $headers .= "Content-Type: text/plain; charset=ISO-8859-1\r\n\r\n";
+	    $headers .= $message."\r\n\r\n";
+
+	    $headers .= "--".md5('boundary2')."--\r\n";
+	    $headers .= "--".md5('boundary1')."\r\n";
+	    $headers .= "Content-Type:  ".$file_type."; ";
+	    $headers .= "name=\"".$file_name."\"\r\n";
+	    $headers .= "Content-Transfer-Encoding:base64\r\n";
+	    $headers .= "Content-Disposition:attachment; ";
+	    $headers .= "filename=\"".$file_name."\"\r\n";
+	    $headers .= "X-Attachment-Id:".rand(1000,9000)."\r\n\r\n";
+	    $headers .= $encoded_content."\r\n";
+	    $headers .= "--".md5('boundary1')."--"; 
 
 		mail($emailTo, $subject, $body, $headers);
 		
